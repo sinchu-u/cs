66307d1a37146ed22ce8daf9c26646bbd61c3e34
Melnychuk Anastasiia <sinchuuuls@gmail.com>
2025-09-24 21:14:43 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetMessage(number));
+        }
+        public static bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static string GetMessage(int number)
+        {
+            if (IsEven(number))
+            {
+                return "Двері відкриваються!";
+            }
+            else return "Двері зачинені...";
+        }
+    }
+}
