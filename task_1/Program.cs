d78774e9ed5e56953f6516ddf2de76b87838d02d
Melnychuk Anastasiia <sinchuuuls@gmail.com>
2026-03-24 23:49:39 +0200

    Add files via upload

---
+++ task_1/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        public class TemperatureChip
+        {
+            public delegate void TemperatureHandler (int temperature);
+            public event TemperatureHandler TemperatureRecived;
+            public void GetTemperature (int temperature)
+            {
+                TemperatureRecived?.Invoke (temperature);
+            }
+        }
+        public class Display
+        {
+            public void DisplayTemperature(int temperature)
+            {
+                Console.WriteLine($"Temperature: {temperature}");
+            }
+            public void Subscribe (TemperatureChip chip)
+            {
+                chip.TemperatureRecived += DisplayTemperature;
+            }
+        }
+        public class AirConditioner
+        {
+            public void OnTemperatureChange(int temperature)
+            {
+                if (temperature <= 17)
+                {
+                    Console.WriteLine("обігрів");
+                }
+                else if (temperature > 17 && temperature < 25)
+                {
+                    Console.WriteLine("кондиціонер вимкнений");
+                }
+                else 
+                {
+                    Console.WriteLine("охолодження");
+                }
+            }
+            public void Subscribe(TemperatureChip chip)
+            {
+                chip.TemperatureRecived += OnTemperatureChange;
+            }
+        }
+        public class SecuritySystem
+        {
+            public void OnCriticTemperature (int temperature)
+            {
+                if (temperature >= 40)
+                {
+                    Console.WriteLine("перегрів");
+                }
+                else if (temperature <= 5)
+                {
+                    Console.WriteLine("перехолодження");
+                }
+            }
+            public void Subscribe(TemperatureChip chip)
+            {
+                chip.TemperatureRecived += OnCriticTemperature;
+            }
+        }
+        static void Main(string[] args)
+        {
+            TemperatureChip chip = new TemperatureChip();
+            Display display = new Display();
+            AirConditioner airConditioner = new AirConditioner();
+            SecuritySystem securitySystem = new SecuritySystem();
+            display.Subscribe(chip);
+            airConditioner.Subscribe(chip);
+            securitySystem.Subscribe(chip);
+            chip.GetTemperature(16);
+            chip.GetTemperature(20);
+            chip.GetTemperature(30);
+            chip.GetTemperature(0);
+            chip.GetTemperature(50);
+        }
+    }
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace task_2
+{
+    internal class Program
+    {
+        public class Player
+        {
+            public delegate void PlayerDamaged(int dmg, int currentHP);
+            public event PlayerDamaged DamageReceived;
+            public int HP { get; private set; } = 100;
+            public void TakeDamage(int dmg)
+            {
+                HP -= dmg;
+                if (HP < 0) HP = 0;
+                DamageReceived?.Invoke(dmg, HP);
+            }
+
+        }
+        public class GameLogger
+        {
+            public void Damage(int dmg, int hp)
+            {
+                Console.WriteLine($"dmg: {dmg}, hp: {hp}");
+            }
+        }
+        public class SoundSystem
+        {
+            public void DamageSound(int dmg, int hp)
+            {
+                if (hp <= 20) Console.WriteLine("big ew");
+                else Console.WriteLine("lil ew");
+            }
+        }
+        public class AchivementSystem
+        {
+            public void DamageAchivements(int dmg, int hp)
+            {
+                if (hp <= 50) Console.WriteLine("half death");
+                else if (hp <= 0) Console.WriteLine("first death");
+            }
+        }
+        public class UIHealthBar
+        {
+            public void ShowHealth(int dmg, int hp)
+            {
+                Console.WriteLine($"HP: {hp}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            GameLogger gameLogger = new GameLogger();
+            SoundSystem soundSystem = new SoundSystem();
+            AchivementSystem achivementSystem = new AchivementSystem();
+            UIHealthBar healthBar = new UIHealthBar();
+
+            player.DamageReceived += gameLogger.Damage;
+            player.DamageReceived += soundSystem.DamageSound;
+            player.DamageReceived += achivementSystem.DamageAchivements;
+            player.DamageReceived += healthBar.ShowHealth;
+
+            player.TakeDamage(10);
+            player.TakeDamage(20);
+            player.TakeDamage(30);
+            player.TakeDamage(25);
+        }
+    }
+}
