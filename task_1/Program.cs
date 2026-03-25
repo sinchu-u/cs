6c694045ec5e1434be5cbfabd80f585124480136
Melnychuk Anastasiia <sinchuuuls@gmail.com>
2026-03-25 12:29:23 +0200

    Add files via upload

---
+++ task_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+
+namespace task_1
+{
+    internal class Program
+    {
+        public delegate string TextOperation(string text);
+        public static string uppercaseText(string text)
+        {
+            return text.ToUpper();
+        }
+        public static string lettersCount(string text)
+        {
+            return $"letters: {text.Length}";
+        }
+        public static string wordsCount(string text)
+        {
+            int count = 0;
+            string[] lines = text.Split('\n');
+            foreach (string line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (text[i] == ' ')
+                    {
+                        count++;
+                    }
+                }
+            }
+                return $"words: {count}";
+        }
+        public static void ProcessFile(string textFile, string resultFile, TextOperation operation)
+        {
+            using (StreamReader sr = new StreamReader(textFile))
+            {
+                string line = sr.ReadToEnd();
+                using (StreamWriter sw = new StreamWriter(resultFile, true))
+                {
+                    sw.WriteLine(operation(line));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string textFile = "textPD21.txt";
+            string resultFile = "resultPD21.txt";
+            TextOperation toUpper = uppercaseText;
+            TextOperation letterCount = lettersCount;
+            TextOperation wordCount = wordsCount;
+            ProcessFile(textFile, resultFile, toUpper);
+            ProcessFile(textFile, resultFile, lettersCount);
+            ProcessFile(textFile, resultFile, wordsCount);
+        }
+    }
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task_2
+{
+    internal class Program
+    {
+        public class MessagePublisher
+        {
+            public delegate void MessageEvent(string text);
+            public event MessageEvent MessageSend;
+            public void Send(string text)
+            {
+                MessageSend.Invoke(text);
+            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
